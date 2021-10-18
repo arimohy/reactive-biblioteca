58bf7917f617783951cdd41060a52e5c56a4cb6b
arimohy <arimohy@gmail.com>
2021-10-18 09:42:33 -0500

    commit inicial

---
+++ src/main/java/com/example/reactivebiblioteca/ReactiveBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivebiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivebiblioteca/ReactiveBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivebiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
