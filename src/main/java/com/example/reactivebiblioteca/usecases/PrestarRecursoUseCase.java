922c3f9999c2641b61afd0cd305f72400fd68913
arimohy <arimohy@gmail.com>
2021-10-18 15:43:23 -0500

    creacion de caso de uso prestar recurso

---
+++ src/main/java/com/example/reactivebiblioteca/usecases/PrestarRecursoUseCase.java
@@ -0,0 +1,39 @@
+package com.example.reactivebiblioteca.usecases;
+
+import com.example.reactivebiblioteca.repositories.Repositorio;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+@Validated
+public class PrestarRecursoUseCase implements Function<String, Mono<String>> {
+    private final Repositorio repositorio;
+    private final ActualizarRecursoUseCase actualizarRecursoUseCase;
+    private final MapperUtils mapperUtils;
+
+    public PrestarRecursoUseCase(ActualizarRecursoUseCase actualizarRecursoUseCase, MapperUtils mapperUtils,Repositorio resourceRepository) {
+        this.repositorio = resourceRepository;
+        this.actualizarRecursoUseCase = new ActualizarRecursoUseCase(mapperUtils, repositorio);
+        this.mapperUtils = mapperUtils;
+    }
+    @Override
+    public Mono<String> apply(String id) {
+        Objects.requireNonNull(id, "el id es requerido");
+        return repositorio.findById(id).flatMap(
+                recurso -> {
+                    if (recurso.getNroEjemplaresPrestados() < recurso.getNroEjemplares()) {
+                        recurso.setFechaprestamo(LocalDate.now());
+                        recurso.setNroEjemplaresPrestados(recurso.getNroEjemplaresPrestados() + 1l);
+                        return actualizarRecursoUseCase.apply(mapperUtils.mapRecursoToDTO().apply(recurso))
+                                .thenReturn("{mensaje: recurso prestado satisfactoriamente}");
+                    }
+                    return Mono.just("{mensaje: No se realizo el prestamo por que todos los ejemplares fueron prestados}");
+                }
+        );
+    }
+}
