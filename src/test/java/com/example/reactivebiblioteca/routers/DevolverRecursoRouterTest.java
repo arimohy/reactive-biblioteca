8b47b23f1617e3bd7036b5bf62277ba795c1ce38
arimohy <arimohy@gmail.com>
2021-10-19 18:46:54 -0500

    test de devolver

--- src/test/java/com/example/reactivebiblioteca/routers/DevolverRecursoRouterTest.java
+++ src/test/java/com/example/reactivebiblioteca/routers/DevolverRecursoRouterTest.java
@@ -1,7 +1,74 @@
 package com.example.reactivebiblioteca.routers;
 
+import com.example.reactivebiblioteca.collections.Recurso;
+import com.example.reactivebiblioteca.model.RecursoDTO;
+import com.example.reactivebiblioteca.repositories.Repositorio;
+import com.example.reactivebiblioteca.usecases.DevolverRecursoUseCase;
+import com.example.reactivebiblioteca.usecases.MapperUtils;
+import com.example.reactivebiblioteca.usecases.PrestarRecursoUseCase;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DevolverRecursoRouter.class, DevolverRecursoUseCase.class, MapperUtils.class})
 
 class DevolverRecursoRouterTest {
+    @MockBean
+    private Repositorio repositorio;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testPrestarRecurso() {
+        Recurso recurso = new Recurso();
+        recurso.setId("xxxxxxx");
+        recurso.setTitulo("titulo1");
+        recurso.setTipoRecurso("libro");
+        recurso.setTematica("gato");
+        recurso.setDisponible("si");
+        recurso.setFechaprestamo(LocalDate.now());
+        recurso.setNroEjemplares(2L);
+        recurso.setNroEjemplaresPrestados(1L);
+        RecursoDTO recursoDTO = new RecursoDTO(
+                recurso.getId(),
+                recurso.getTitulo(),
+                recurso.getTipoRecurso(),
+                recurso.getTematica(),
+                recurso.getDisponible(),
+                recurso.getFechaprestamo(),
+                recurso.getNroEjemplares(),
+                recurso.getNroEjemplaresPrestados()
+        );
+
+        Mono<Recurso> recursoMono = Mono.just(recurso);
+        when(repositorio.findById(recurso.getId())).thenReturn(recursoMono);
+        when(repositorio.save(any())).thenReturn(recursoMono);
+
+        webTestClient.get()
+                .uri("/recursos/devolver/{id}", "xxxxxxx")
+                .exchange()
+                .expectStatus().isAccepted()
+                .expectBody(String.class)
+                .value(userResponse -> {
+                            Assertions.assertThat(userResponse).isEqualTo("{ Mensaje : El recurso fue devuelto satisfactoriamente }");
+                        }
+                );
+    }
 
 }
